fd02ad9efbaf5ac8c8e42608a3605397e1e1b9d8
Inabah khan <inabahk@gmail.com>
2026-02-10 18:35:03 +0530

    Implement logging to monitor_log.csv
    Add logging functionality to monitor results in a CSV file.

--- logger.py
+++ logger.py
@@ -1,2 +1,28 @@
-python main.py
+import csv
+import os
+from datetime import datetime
 
+FILE_NAME = "monitor_log.csv"
+
+def log_result(url, status, latency, error=""):
+    file_exists = os.path.isfile(FILE_NAME)
+
+    with open(FILE_NAME, mode="a", newline="", encoding="utf-8") as f:
+        writer = csv.writer(f)
+
+        if not file_exists:
+            writer.writerow([
+                "timestamp",
+                "url",
+                "status",
+                "latency_seconds",
+                "error"
+            ])
+
+        writer.writerow([
+            datetime.now().isoformat(),
+            url,
+            status,
+            latency,
+            error
+        ])
